79e2d435e1e2a2daf67dbfbb68064a1c10e0a08a
Dmitry Shulyak <yashulyak@gmail.com>
2024-04-06 09:21:35 +0200

    parametrize play_4

--- play_4.sh
+++ play_4.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-play run \
-    -e PATH=./target/debug \
-    -e RUST_LOG=debug \
+RUST_LOG=${RUST_LOG:-info}
+RELEASE=${RELEASE:-debug}
+BINARY_PATH=${BINARY_PATH:-$(pwd)/target/$RELEASE}
+
+RUST_LOG=$RUST_LOG PATH=$PATH:$BINARY_PATH play run \
+    -e RUST_LOG=$RUST_LOG \
+    -w /tmp/example \
     --redirect \
-    -w /tmp/first \
     -c 'example run -d dir0 -k 0.key -p public_keys -c 10.0.0.2:9000 -c 10.0.0.3:9000 -c 10.0.0.4:9000' \
     -c 'example run -d dir1 -k 1.key -p public_keys -c 10.0.0.1:9000 -c 10.0.0.3:9000 -c 10.0.0.4:9000' \
     -c 'example run -d dir2 -k 2.key -p public_keys -c 10.0.0.1:9000 -c 10.0.0.2:9000 -c 10.0.0.4:9000' \
