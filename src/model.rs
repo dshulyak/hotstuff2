6a5766f9d4defdb3145ec138ccbbdb40d8a0bac9
Dmitry Shulyak <yashulyak@gmail.com>
2024-04-10 19:41:56 +0200

    refactor to nicer model

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,17 @@
+// ideas for the model based on [Twins: BFT Systems Made Robust](https://drops.dagstuhl.de/storage/00lipics/lipics-vol217-opodis2021/LIPIcs.OPODIS.2021.7/LIPIcs.OPODIS.2021.7.pdf)
+
+
 use std::{
     cell::RefCell,
     collections::{BTreeMap, HashMap},
 };
 
-use itertools::Itertools;
+use arbitrary::Arbitrary;
 use bit_vec::BitVec;
+use itertools::Itertools;
 
 use crate::{
-    sequential::{Action, Actions, Consensus},
+    sequential::{Action, Actions, Consensus, OnDelay, OnMessage, Proposer},
     types::{AggregateSignature, Block, Certificate, PrivateKey, PublicKey, Vote, ID},
 };
 
@@ -74,9 +78,25 @@ pub enum Op {
     Advance,
 }
 
+impl<'a> Arbitrary<'a> for Op {
+    fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
+        if u.ratio(1, 10)? {
+            // generate all nodes into single array
+            // split array into 1-3 random partitions
+            // only 1 twin can exist in the partition, so any 2nd twin is moved to the next one
+            // if partition doesn't exist creeate empty partition
+
+            let honest = (0..5).map(|i| Node::Honest(i));
+            let twins = (5..7)
+                .flat_map(|i| (0..=0).map(|j| Node::Twin(i, j)).collect::<Vec<Node>>());
+            Ok(Op::Routes(vec![honest.chain(twins).collect()]))
+        } else {
+            Ok(Op::Advance)
+        }
+    }
+}
+
 pub struct Model {
-    total: usize,
-    twins: usize,
     consensus: HashMap<Node, Consensus<Sink>>,
     public_key_to_node: HashMap<PublicKey, Node>,
     commits: BTreeMap<u64, HashMap<Node, Block>>,
@@ -125,8 +145,6 @@ impl Model {
             .flat_map(|(n, c)| c.public_keys().into_iter().map(|(_, public)| (public, *n)))
             .collect::<HashMap<_, _>>();
         Self {
-            total: total,
-            twins: twins,
             consensus: nodes,
             public_key_to_node: by_public,
             commits: BTreeMap::new(),
@@ -137,21 +155,103 @@ impl Model {
     }
 
     pub fn step(&mut self, op: Op) -> anyhow::Result<()> {
+        match op {
+            Op::Routes(partition) => {
+                self.paritition(partition);
+            }
+            Op::Advance => {
+                self.advance();
+            }
+        }
+        self.verify()
+    }
+
+    fn advance(&mut self) {
+        if self.routes.is_none() {
+            return;
+        }
+        let routes = self.routes.as_ref().unwrap();
+        for (id, consensus) in self.consensus.iter() {
+            for action in consensus.sink().drain() {
+                match action {
+                    Action::StateChange(change) => {
+                        if let Some(cert) = change.commit {
+                            self.commits
+                                .entry(cert.block.height)
+                                .or_insert_with(HashMap::new)
+                                .insert(*id, cert.block.clone());
+                        }
+                        if let Some(lock) = change.lock {
+                            self.locks.insert(*id, lock);
+                        }
+                    }
+                    Action::Send(msg, target) => {
+                        if let Some(target) = target {
+                            _ = self
+                                .consensus
+                                .get(self.public_key_to_node.get(&target).unwrap())
+                                .unwrap()
+                                .on_message(msg.clone());
+                        } else {
+                            for linked in routes.get(id).iter() {
+                                _ = self.consensus.get(linked).unwrap().on_message(msg.clone());
+                            }
+                            _ = consensus.on_message(msg.clone());
+                        }
+                    }
+                    Action::Propose => {
+                        let nonce = self.proposals_counter.get(id).unwrap_or(&0);
+                        let mut block_id = [0u8; 32];
+                        block_id[0..8].copy_from_slice(&nonce.to_be_bytes());
+                        block_id[9] = id.num();
+                        _ = consensus.propose(block_id.into());
+                        self.proposals_counter.insert(*id, nonce + 1);
+                    }
+                }
+            }
+        }
+        for (_, consensus) in self.consensus.iter() {
+            consensus.on_delay();
+        }
+    }
+
+    fn verify(&self) -> anyhow::Result<()> {
+        if let Some(mut values) = self
+            .commits
+            .last_key_value()
+            .map(|(_, v)| v.values().into_iter())
+        {
+            let first = values.next().unwrap();
+            for other in values {
+                anyhow::ensure!(first == other, "commits are not equal");
+            }
+        }
         Ok(())
     }
 
-    pub fn advance(&mut self) {
-
+    fn paritition(&mut self, partition: Vec<Vec<Node>>) {
+        match self.routes.take() {
+            None => {
+                self.routes = Some(
+                    partition
+                        .iter()
+                        .flat_map(|side| {
+                            side.iter().permutations(2).map(|pair| (*pair[0], *pair[1]))
+                        })
+                        .collect::<HashMap<_, _>>(),
+                );
+            }
+            Some(_) => {
+                self.routes = Some(
+                    partition
+                        .iter()
+                        .flat_map(|side| {
+                            side.iter().permutations(2).map(|pair| (*pair[0], *pair[1]))
+                        })
+                        .collect::<HashMap<_, _>>(),
+                );
+                // TODO restored connections should sync latest commits and locks from each other
+            }
+        }
     }
-
-    pub fn paritition(&mut self, partition: Vec<Vec<Node>>) {
-        self.routes = Some(
-            partition
-                .iter()
-                .flat_map(|side| {
-                    side.iter().permutations(2).map(|pair| (*pair[0], *pair[1]))
-                })
-                .collect::<HashMap<_, _>>(),
-        );
-    }   
 }
